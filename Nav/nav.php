860e0c3596c4e01cc6442e5b4ebd2f7de89c0d96
AKHTAR Hassan <120174032+Eskoop@users.noreply.github.com>
2024-03-15 18:36:07 +0100

    15/03(3)

--- Nav/nav.php
+++ Nav/nav.php
@@ -1,14 +1,10 @@
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
-<link rel="stylesheet" href="asset/style.css" /> -->
-
-
 
 <header class="header bg-white">
   <nav class="navbar">
     <a href="index.php" class="logo">AKHTAR Hassan</a>
     <div class="nav-links mobile-menu">
       <ul>
-        <li><a href="index.php"  class="active">Accueil</a></li>
+        <li><a href="index.php"  >Accueil</a></li>
         <li><a href="">A Propos</a></li>
         <li><a href="">Projet</a></li>
         <li><a href="contact.php">Contact</a></li>
